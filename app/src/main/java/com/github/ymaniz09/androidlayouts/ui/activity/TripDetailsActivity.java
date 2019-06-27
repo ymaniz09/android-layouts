9916167977c9fe425ae21ad56ff9783a4a63f7e0
Ygor <ygor.maniz@gmail.com>
2019-06-27 18:14:13 -0300

    Refactoring

--- app/src/main/java/com/github/ymaniz09/androidlayouts/ui/activity/TripDetailsActivity.java
+++ app/src/main/java/com/github/ymaniz09/androidlayouts/ui/activity/TripDetailsActivity.java
@@ -12,6 +12,7 @@ import com.github.ymaniz09.androidlayouts.util.PluralUtil;
 import com.github.ymaniz09.androidlayouts.util.ResourcesUtil;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 public class TripDetailsActivity extends AppCompatActivity {
 
@@ -20,21 +21,47 @@ public class TripDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_details);
 
+        setTitle(getString(R.string.trip_details_activity_title));
+
         Trip trip = new Trip("São Paulo", "sao_paulo_sp",
                 2, new BigDecimal("243.99"));
 
-        TextView place = findViewById(R.id.trip_details_destination);
-        place.setText(trip.getPlace());
+        setPlace(trip);
 
-        ImageView image = findViewById(R.id.trip_details_image);
+        setImage(trip);
 
-        image.setImageDrawable(ResourcesUtil.getDrawable(this, trip.getImage()));
+        setDays(trip);
 
-        TextView days = findViewById(R.id.trip_details_days);
-        days.setText(PluralUtil.getPlural(this, trip.getDays(), R.plurals.numberOfDays));
+        setPrice(trip);
 
+        setDates();
+    }
+
+    private void setDates() {
+        TextView dates = findViewById(R.id.trip_details_dates);
+        OffsetDateTime now = OffsetDateTime.now();
+        dates.setText(String.format("%s/%s/%s - %s/%s/%s",
+                now.getYear(), now.getMonthValue(), now.getDayOfMonth(),
+                now.getYear(), now.getMonthValue(), now.plusDays(5).getDayOfMonth()));
+    }
+
+    private void setPrice(Trip trip) {
         TextView price = findViewById(R.id.trip_details_price);
         price.setText(FormattingUtil.formatBigDecimalToUSCurrency(trip.getValue()));
-        
+    }
+
+    private void setDays(Trip trip) {
+        TextView days = findViewById(R.id.trip_details_days);
+        days.setText(PluralUtil.getPlural(this, trip.getDays(), R.plurals.numberOfDays));
+    }
+
+    private void setImage(Trip trip) {
+        ImageView image = findViewById(R.id.trip_details_image);
+        image.setImageDrawable(ResourcesUtil.getDrawable(this, trip.getImage()));
+    }
+
+    private void setPlace(Trip trip) {
+        TextView place = findViewById(R.id.trip_details_destination);
+        place.setText(trip.getPlace());
     }
 }
